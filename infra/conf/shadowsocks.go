43ac85f94b4c5e157d06b5e290f798a288f7bca3
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2023-07-11 10:27:42 +0900

    feat/shadowsocks: add some junk

--- infra/conf/shadowsocks.go
+++ infra/conf/shadowsocks.go
@@ -13,23 +13,6 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-func cipherFromString(c string) shadowsocks.CipherType {
-	switch strings.ToLower(c) {
-	case "aes-128-gcm", "aead_aes_128_gcm":
-		return shadowsocks.CipherType_AES_128_GCM
-	case "aes-256-gcm", "aead_aes_256_gcm":
-		return shadowsocks.CipherType_AES_256_GCM
-	case "chacha20-poly1305", "aead_chacha20_poly1305", "chacha20-ietf-poly1305":
-		return shadowsocks.CipherType_CHACHA20_POLY1305
-	case "xchacha20-poly1305", "aead_xchacha20_poly1305", "xchacha20-ietf-poly1305":
-		return shadowsocks.CipherType_XCHACHA20_POLY1305
-	case "none", "plain":
-		return shadowsocks.CipherType_NONE
-	default:
-		return shadowsocks.CipherType_UNKNOWN
-	}
-}
-
 type ShadowsocksUserConfig struct {
 	Cipher   string   `json:"method"`
 	Password string   `json:"password"`
@@ -61,7 +44,7 @@ func (v *ShadowsocksServerConfig) Build() (proto.Message, error) {
 		for _, user := range v.Users {
 			account := &shadowsocks.Account{
 				Password:   user.Password,
-				CipherType: cipherFromString(user.Cipher),
+				CipherType: shadowsocks.CipherFromString(user.Cipher),
 				IvCheck:    v.IVCheck,
 			}
 			if account.Password == "" {
@@ -80,7 +63,7 @@ func (v *ShadowsocksServerConfig) Build() (proto.Message, error) {
 	} else {
 		account := &shadowsocks.Account{
 			Password:   v.Password,
-			CipherType: cipherFromString(v.Cipher),
+			CipherType: shadowsocks.CipherFromString(v.Cipher),
 			IvCheck:    v.IVCheck,
 		}
 		if account.Password == "" {
@@ -172,7 +155,9 @@ type ShadowsocksServerTarget struct {
 }
 
 type ShadowsocksClientConfig struct {
-	Servers []*ShadowsocksServerTarget `json:"servers"`
+	Servers    []*ShadowsocksServerTarget `json:"servers"`
+	Plugin     string                     `json:"plugin"`
+	PluginOpts string                     `json:"pluginOpts"`
 }
 
 func (v *ShadowsocksClientConfig) Build() (proto.Message, error) {
@@ -222,7 +207,7 @@ func (v *ShadowsocksClientConfig) Build() (proto.Message, error) {
 		account := &shadowsocks.Account{
 			Password: server.Password,
 		}
-		account.CipherType = cipherFromString(server.Cipher)
+		account.CipherType = shadowsocks.CipherFromString(server.Cipher)
 		if account.CipherType == shadowsocks.CipherType_UNKNOWN {
 			return nil, errors.New("unknown cipher method: ", server.Cipher)
 		}
@@ -245,6 +230,8 @@ func (v *ShadowsocksClientConfig) Build() (proto.Message, error) {
 	}
 
 	config.Server = serverSpecs
+	config.Plugin = v.Plugin
+	config.PluginOpts = v.PluginOpts
 
 	return config, nil
 }
